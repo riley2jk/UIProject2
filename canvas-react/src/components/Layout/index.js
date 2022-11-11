b8cabb49c3c40aa4cc912e3f0da13e623a176dd1
riley2jk <65510000+riley2jk@users.noreply.github.com>
2022-11-11 14:41:41 -0500

    To-Do ready for dev

--- canvas-react/src/components/Layout/index.js
+++ canvas-react/src/components/Layout/index.js
@@ -1,12 +1,19 @@
-import './index.scss';
-import SideNav from '../SideNav'
+import "./index.scss";
+import SideNav from "../SideNav";
+import ToDo from "../ToDo";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-    return (
-        <>
-            <SideNav />
-        </>
-    )
-}
+  return (
+    <div className="App">
+      <SideNav />
+      <div className="page">
+        <span className="page-bar" />
+        <Outlet />
+      </div>
+      <ToDo />
+    </div>
+  );
+};
 
-export default Layout  
+export default Layout;

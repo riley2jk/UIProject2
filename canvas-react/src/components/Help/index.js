4d6c4bb60ae7d7e18fab2456123e2ba8a8445aaa
riley2jk <65510000+riley2jk@users.noreply.github.com>
2022-11-13 23:57:14 -0500

    Help Page

--- canvas-react/src/components/Help/index.js
+++ canvas-react/src/components/Help/index.js
@@ -1,7 +1,54 @@
 import "./index.scss";
 
 const Help = () => {
-  return <div className="title">Help</div>;
+    return (
+        <>
+            <div className="title">Help</div>
+            <h1 className="links-title">Useful Links</h1>
+            <div className="help-links">
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://community.canvaslms.com/t5/Canvas/ct-p/canvas"
+                >
+                    Search the Canvas Guides
+                </a>
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://cases.canvaslms.com/liveagentchat?chattype=admin"
+                >
+                    Live Chat with Canvas Support (Faculty)
+                </a>
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://cases.canvaslms.com/liveagentchat?chattype=student"
+                >
+                    Live Chat with Canvas Support (Students)
+                </a>
+                <a target="_blank" rel="noreferrer" href="https://kb.uc.edu/">
+                    IT@UC Knowledge Base
+                </a>
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://uc.edu/ithelp"
+                >
+                    IT@UC General IT Service Desk
+                </a>
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://libraries.uc.edu/"
+                >
+                    UC Libraries
+                </a>
+            </div>
+            <h1>Other Useful Information</h1>
+            <p>Canvas Support Hotline (Students): 1 (855) 745-4413</p>
+        </>
+    );
 };
 
 export default Help;
